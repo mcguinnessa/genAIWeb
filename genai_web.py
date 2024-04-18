bb0c1603f4b480fdb82e91c0db5893614cb7111d
Alex McGuinness <github@alexmcguinness.me.uk>
2024-04-18 12:08:35 +0100

    Add in ability to select LLM

--- genai_web.py
+++ genai_web.py
@@ -29,6 +29,13 @@ MODEL = "mistral.mixtral-8x7b-instruct-v0:1"
 #MODEL = "amazon.titan-tg1-large"
 
 
+model_dict = {"mistral.mixtral-8x7b-instruct-v0:1" : 4096,
+              "meta.llama2-70b-chat-v1" : 2048,
+              "ai21.j2-ultra-v1" : 4096,
+              "amazon.titan-tg1-large" : 4096 }
+            
+
+
 HEADING_NO = "No."
 HEADING_NAME = "Test Name"
 HEADING_DESC = "Description"
@@ -60,7 +67,7 @@ def validate_service(input):
 # Generates the tests
 #
 ############################################################
-def generate_tests(element, service, format, temperature, topp, max_tokens, num_tests,
+def generate_tests(model, element, service, format, temperature, topp, max_tokens, num_tests,
                    role, type):
 
   if not validate_element(element):
@@ -86,10 +93,10 @@ def generate_tests(element, service, format, temperature, topp, max_tokens, num_
   formatting_suffix = ""
 
   if format == "HTML":
-    #format = "The generated test cases must be presented as a HTML table using the <table> tag. Each test case must be a row in the table, delimited with the <tr></tr> tag; each of the headings will be a column, delimited with the <th></th> tag. You cannot use the | character. This table must be asthetically pleasing and easy to read"
-    format = "Each genrated test cases must be presented as row which will be added to a HTML table. Each row will be prefixed with the <tr> tag and suffixed with the </tr> tag; each of the headings will be a column, prefixed with the <th> tag and suffixed with the </th> tag. This table must be asthetically pleasing and easy to read. Do not use the tag <table>."
+    format = "Each test cases must be presented as row which can be added to a HTML table. Each row will be prefixed with <tr> & suffixed with </tr>; each of the headings will be a column, prefixed with <th> & suffixed with </th>. This table must easy to read. Do not include the tag <table>"
     formatting_prefix = "<table>"
     formatting_suffix = "</table>"
+
   elif format == "JSON":
     format = "The resultant test cases must be presented in JSON format. Each test case wil be a JSON object in the table, and each of the headings will be a key value pair."
   elif format == "CSV":
@@ -99,29 +106,29 @@ def generate_tests(element, service, format, temperature, topp, max_tokens, num_
   elif format == "Text":
     format = "The resultant test cases must be presented in the format of a table in plain text"
 
-  prompt = f"""You are a {role}. Generate unique test cases for {element} including the service {service} based on your knowledge. 
+  prompt = f"""You are a {role}. Generate {num_tests_to_ask_for} unique test cases for {element} including the for the service service {service}. 
 
-   There should be {num_tests_to_ask_for} test cases.
-
-   The test cases must include {type} test cases.
+   The test cases must be {type} test cases.
 
    {format}
    
-   The test cases must have the following column headings: {HEADING_NO}, {HEADING_NAME}, {HEADING_DESC}, {HEADING_ID}, {HEADING_PRE}, {HEADING_STEPS}, and {HEADING_RESULTS}. 
+   The test cases must include the following elements, each corresponding to a heading: {HEADING_NO}, {HEADING_NAME}, {HEADING_DESC}, {HEADING_ID}, {HEADING_PRE}, {HEADING_STEPS}, and {HEADING_RESULTS}. 
 
-   '{HEADING_NO}' is shorthand for number. This should be a unique integer for each test case, starting from 1 and incrementing by for each test case. 
-   
-   The {HEADING_NAME} should be a useful short description of the test. The {HEADING_DESC} should be summary of the test and should end in -{element}. The {HEADING_ID} must be an alpha-numeric key, unique for each test. The {HEADING_PRE} field must describe and preconditions needed before the tests can be executed. The {HEADING_STEPS} field must describe clearly how to execute the test and each step shall be numbered. The {HEADING_RESULTS} field must correspond to the test steps, with an expected outcome for each step enumberated step. 
+   {HEADING_NO}' is an abbreviation for number. This is a unique integer for each test case, starting from 1 and incrementing by 1 for each test case. 
+   {HEADING_NAME} is a useful short description of the test. 
+   {HEADING_DESC} is a summary of the test and should end in -{element}. 
+   {HEADING_ID} is an alpha-numeric ID, unique for each case. 
+   {HEADING_PRE} describes the preconditions needed for the tests to be executed. 
+   {HEADING_STEPS} is a series of steps that clearly describes how to execute the test case. Each step must be numbered. 
+   {HEADING_RESULTS} describes the expected outcome for each step itemised in {HEADING_STEPS}.
 
    Do not use the , character in any of the output.
-   
    """
-  
 
   tests_remaining = num_tests
   output = ""
   while tests_remaining > 0:
-    query_output = send_query(prompt, session_id, temperature, topp, max_tokens)
+    query_output = send_query(model, prompt, session_id, temperature, topp, max_tokens)
     print("QOUTPUT:" + query_output)
     output += query_output
 
@@ -130,7 +137,7 @@ def generate_tests(element, service, format, temperature, topp, max_tokens, num_
       num_tests_to_ask_for = TESTS_PER_CALL
     else:
       num_tests_to_ask_for = tests_remaining
-    prompt = f"Generate {num_tests_to_ask_for} more unique test cases using the same requirements. All of the same fields must be included. The fields {HEADING_NO} and {HEADING_ID} should continue incrementing from the last logical integer"
+    prompt = f"Generate {num_tests_to_ask_for} more unique test cases using the same requirements and in the same format. All of the same fields must be included. The fields {HEADING_NO} and {HEADING_ID} should continue incrementing from the last logical integer"
 
   return f"{formatting_prefix}{output}{formatting_suffix}"
 
@@ -139,12 +146,14 @@ def generate_tests(element, service, format, temperature, topp, max_tokens, num_
 # Sends the query to the playground
 #
 ############################################################
-def send_query(prompt, session_id, temperature, topp, max_tokens):
+def send_query(model, prompt, session_id, temperature, topp, max_tokens):
 
+  print("Model         :" + str(model))
+  print("Max Tokens    :" + str(max_tokens))
   print("Session ID IN :" + str(session_id))
-  print("Temperature :" + str(temperature))
-  print("TopP        :" + str(topp))
-  print("Prompt      :" + str(prompt))
+  print("Temperature   :" + str(temperature))
+  print("TopP          :" + str(topp))
+  print("Prompt        :" + str(prompt))
 
   data = {
       "action": "run",
@@ -153,7 +162,8 @@ def send_query(prompt, session_id, temperature, topp, max_tokens):
           "mode": "chain",
           "text": prompt,
           "files": [],
-          "modelName": MODEL,
+          #"modelName": MODEL,
+          "modelName": model,
           "provider": "bedrock",
           "sessionId": str(session_id),
           "workspaceId": WORKSPACE_ID,
@@ -187,6 +197,16 @@ def send_query(prompt, session_id, temperature, topp, max_tokens):
   return r1
 
 
+##################################################################################
+#
+# Method that changes the default value of Max Tokens based on the model selected
+#
+##################################################################################
+def change_max_token_default(model_name):
+   #print("Model:" + str(model_name))
+   number = model_dict[model_name] 
+   #print("Number:" + str(number))
+   return gr.Number(value=number, label="Max Tokens", scale=1)
    
 
 #########################################################################################################
@@ -204,8 +224,8 @@ if __name__ == "__main__":
   #theme = gr.themes.Monochrome()
 
   #prompt_template = """generate test cases for #network element including #service."""
-  prompt_element_template = """#element."""
-  prompt_subsystem_template = """#service."""
+  prompt_element_template = """element"""
+  prompt_subsystem_template = """service"""
 
   url = SOCKET_URL
   ws = websocket.create_connection(url, header={"x-api-key": API_TOKEN})
@@ -219,24 +239,29 @@ if __name__ == "__main__":
        element = gr.Textbox(label="Generate tests for ", value=prompt_element_template, scale=2)
        subsystem = gr.Textbox(label="Service ", value=prompt_subsystem_template, scale=1)
 
+#    gr.Label("LLM")
     with gr.Row() as row2:
-      format = gr.Dropdown(choices=["HTML", "CSV", "Excel", "JSON", "Text"],
+       default_max_tokens = 2048
+       model = gr.Dropdown(choices=model_dict.keys(), value=list(model_dict.keys())[0], label="Model", scale=2)
+       temperature = gr.Number(value=0.4, label="Temperature", scale=1)
+       topp = gr.Number(value=0.9, label="TopP", scale=1)
+       max_tokens = gr.Number(value=4096, label="Max Tokens", scale=1)
+       model.select(fn=change_max_token_default, inputs=model, outputs=max_tokens)
+
+    with gr.Row() as row3:
+       num_tests = gr.Number(value=10, label="Number")
+       format = gr.Dropdown(choices=["HTML", "CSV", "Excel", "JSON", "Text"],
                            label="Format",
                            value="HTML")
-      temperature = gr.Number(value=0.4, label="Temperature")
-      topp = gr.Number(value=0.9, label="TopP")
-      max_tokens = gr.Number(value=4096, label="Max Tokens")
-    with gr.Row() as row3:
-      num_tests = gr.Number(value=10, label="Number")
 
-      role = gr.Dropdown(
+       role = gr.Dropdown(
           choices=["Tester", "Software Engineer", "Customer", "Analyst"],
           value="Tester",
           label="Role")
-      type = gr.Dropdown(choices=[
+       type = gr.Dropdown(choices=[
           "Sunny Day", "Rainy Day", "Functional", "High Availability",
           "Resilience", "Acceptance"
-      ],
+       ],
                          value="Functional",
                          label="Test Type")
 
@@ -247,19 +272,11 @@ if __name__ == "__main__":
 
     gen_btn.click(fn=generate_tests,
                   inputs=[
-                      element, subsystem, format, temperature, topp, max_tokens, num_tests,
+                      model, element, subsystem, format, temperature, topp, max_tokens, num_tests,
                       role, type
                   ],
                   outputs=html_block,
                   api_name="TCGen")
-
-#    iface = gr.Interface(
-#      fn=send_query,
-#      inputs=["text"],
-#      outputs="text",
-#      title="Test Generator",
-#      description="Describe the test cases"
-#    )
 
   demo.launch(share=True)
   ws.close()
